b52a07e6208f30fdf07d77bece08cf82009f60e1
Pavel Chernov <riordanfols@gmail.com>
2021-03-19 00:49:04 +0300

    Start

---
+++ src/test/java/ru/chernov/diplom/DiplomApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.chernov.diplom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
